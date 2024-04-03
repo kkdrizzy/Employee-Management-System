037719eaa93bb4f09914234cc11eb704d1e5556d
kkdrizzy <kaustubhkatti.205@gmail.com>
2024-04-03 18:04:45 +0100

    Uploading local projects

---
+++ ems-backend/src/main/java/example/ems/mapper/DepartmentMapper.java
@@ -0,0 +1,25 @@
+package example.ems.mapper;
+
+import example.ems.dto.DepartmentDto;
+import example.ems.entity.Department;
+
+public class DepartmentMapper {
+
+    // convert department jpa entity into department dto
+    public static DepartmentDto mapToDepartmentDto(Department department){
+        return new DepartmentDto(
+                department.getId(),
+                department.getDepartmentName(),
+                department.getDepartmentDescription()
+        );
+    }
+
+    // convert department dto into department jpa entity
+    public static Department mapToDepartment(DepartmentDto departmentDto){
+        return new Department(
+                departmentDto.getId(),
+                departmentDto.getDepartmentName(),
+                departmentDto.getDepartmentDescription()
+        );
+    }
+}
